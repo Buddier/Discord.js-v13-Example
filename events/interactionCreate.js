6661b4c021fffa76da1b17e9159627d3321c63fe
Cristhian <79179637+Expectatives@users.noreply.github.com>
2021-12-02 15:26:12 -0500

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,31 +4,24 @@ const client = require("../index");
 
 client.on("interactionCreate", async (interaction) => {
   if (interaction.isCommand()) {
-        await interaction.deferReply({ ephemeral: false }).catch(() => {});
+    await interaction.deferReply({ ephemeral: false }).catch(() => {});
 
-        const command = client.slashCommands.get(interaction.commandName);
-        if (!command) return interaction.followUp({ content: 'an Erorr' });
+    const command = client.slashCommands.get(interaction.commandName);
+    if (!command) return interaction.followUp({ content: 'an Erorr' });
 
-        const args = [];
+    const args = [];
 
-        for (let option of interaction.options.data) {
-            if (option.type === 'SUB_COMMAND') {
-                if (option.name) args.push(option.name);
-                option.options?.forEach(x =>  {
-                    if (x.value) args.push(x.value);
-                });
-            } else if (option.value) args.push(option.value);
-        }
-
-        try {
-
-            command.run(client, interaction, args)
-
-        } catch (e) {
-
-            interaction.followUp({ content: e.message });
-
-
-        }
+    for (let option of interaction.options.data) {
+      if (option.type === 'SUB_COMMAND') {
+        if (option.name) args.push(option.name);
+          option.options?.forEach(x =>  {
+            if (x.value) args.push(x.value);
+        });
+      } else if (option.value) args.push(option.value);
+    } try {
+      command.run(client, interaction, args)
+    } catch (e) {
+      interaction.followUp({ content: e.message });
+    }
   }
 });
