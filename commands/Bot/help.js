b9baaca094a498957923cf4685f7a32f5bc3c3c3
Expectatives <79179637+Expectatives@users.noreply.github.com>
2022-03-12 21:12:49 -0500

    Fix prefix commands

--- commands/Bot/help.js
+++ commands/Bot/help.js
@@ -1,86 +1,87 @@
-const {readdirSync} = require("fs");
+const { readdirSync } = require("fs");
 
 // Example of how to make a Help Command
 
 module.exports = {
-  name: "help",
-  aliases: ["h", "commands"],
-  usage: '!help <command>',
-  category: "Bot",
-  description: "Return all commands, or one specific command!",
-  run: async (client, message, args) => {
+    name: "help",
+    aliases: ["h", "commands"],
+    usage: '!help <command>',
+    category: "Bot",
+    description: "Return all commands, or one specific command!",
+    ownerOnly: false,
+    run: async (client, message, args) => {
 
-    // Buttons that take you to a link
-    // If you want to delete them, remove this part of
-    // the code and in line: 55 delete ", components: [row]"
-    const row = new client.discord.MessageActionRow()
-	.addComponents(
-	new client.discord.MessageButton()
-          .setLabel("GitHub")
-          .setStyle("LINK")
-          .setURL("https://github.com/Expectatives/Discord.js-v13-Example"),
-        new client.discord.MessageButton()
-          .setLabel("Support")
-          .setStyle("LINK")
-          .setURL("https://dsc.gg/faithcommunity")
-    );
-    
-    if (!args[0]) {
+        // Buttons that take you to a link
+        // If you want to delete them, remove this part of
+        // the code and in line: 55 delete ", components: [row]"
+        const row = new client.discord.MessageActionRow()
+            .addComponents(
+                new client.discord.MessageButton()
+                    .setLabel("GitHub")
+                    .setStyle("LINK")
+                    .setURL("https://github.com/Expectatives/Discord.js-v13-Example"),
+                new client.discord.MessageButton()
+                    .setLabel("Support")
+                    .setStyle("LINK")
+                    .setURL("https://dsc.gg/faithcommunity")
+            );
 
-      // Get the commands of a Bot category
-      const botCommandsList = [];
-      readdirSync(`./commands/Bot`).forEach((file) => {
-        const filen = require(`../../commands/Bot/${file}`);
-        const name = `\`${filen.name}\``
-        botCommandsList.push(name);
-      });
+        if (!args[0]) {
 
-      // Get the commands of a Utility category
-      const utilityCommandsList = [];
-      readdirSync(`./commands/Utility`).forEach((file) => {
-        const filen = require(`../../commands/Utility/${file}`);
-        const name = `\`${filen.name}\``
-        utilityCommandsList.push(name);
-      });
+            // Get the commands of a Bot category
+            const botCommandsList = [];
+            readdirSync(`./commands/Bot`).forEach((file) => {
+                const filen = require(`../../commands/Bot/${file}`);
+                const name = `\`${filen.name}\``
+                botCommandsList.push(name);
+            });
 
-      // This is what it commands when using the command without arguments
-      const helpEmbed = new client.discord.MessageEmbed()
-        .setTitle(`${client.user.username} Help`)
-        .setDescription(` Hello **<@${message.author.id}>**, I am <@${client.user.id}>.  \nYou can use \`!help <command>\` to see more info about the commands!\n**Total Commands:** ${client.commands.size}\n**Total SlashCommands:** ${client.slashCommands.size}`)
-        .addField("🤖 - Bot Commands", botCommandsList.map((data) => `${data}`).join(", "), true)
-	.addField("🛠 - Utility Commands", utilityCommandsList.map((data) => `${data}`).join(", "), true)
-        .setColor(client.config.embedColor)
-        .setFooter(client.config.embedfooterText, client.user.avatarURL());
-      
-      message.reply({embeds: [helpEmbed], allowedMentions: {repliedUser: false}, components: [row]});
-    } else {
-      const command = client.commands.get(args[0].toLowerCase()) || client.commands.find((c) => c.aliases && c.aliases.includes(args[0].toLowerCase()));
+            // Get the commands of a Utility category
+            const utilityCommandsList = [];
+            readdirSync(`./commands/Utility`).forEach((file) => {
+                const filen = require(`../../commands/Utility/${file}`);
+                const name = `\`${filen.name}\``
+                utilityCommandsList.push(name);
+            });
 
-      // This is what it sends when using the command with argument and it does not find the command
-      if (!command) {
-        message.reply({content: `There isn't any command named "${args[0]}"`, allowedMentions: {repliedUser: false}});
-      } else {
+            // This is what it commands when using the command without arguments
+            const helpEmbed = new client.discord.MessageEmbed()
+                .setTitle(`${client.user.username} Help`)
+                .setDescription(` Hello **<@${message.author.id}>**, I am <@${client.user.id}>.  \nYou can use \`!help <command>\` to see more info about the commands!\n**Total Commands:** ${client.commands.size}\n**Total SlashCommands:** ${client.slash.size}`)
+                .addField("🤖 - Bot Commands", botCommandsList.map((data) => `${data}`).join(", "), true)
+                .addField("🛠 - Utility Commands", utilityCommandsList.map((data) => `${data}`).join(", "), true)
+                .setColor(client.config.embedColor)
+                .setFooter({ text: `${client.config.embedfooterText}`, iconURL: `${client.user.displayAvatarURL()}` });
 
-        // This is what it sends when using the command with argument and if it finds the command
-        let command = client.commands.get(args[0].toLowerCase()) || client.commands.find((c) => c.aliases && c.aliases.includes(args[0].toLowerCase()));
-        let name = command.name;
-        let description = command.description || "No descrpition provided"
-        let usage = command.usage || "No usage provided"
-        let aliases = command.aliases || "No aliases provided"
-        let category = command.category || "No category provided!"
+            message.reply({ embeds: [helpEmbed], allowedMentions: { repliedUser: false }, components: [row] });
+        } else {
+            const command = client.commands.get(args[0].toLowerCase()) || client.commands.find((c) => c.aliases && c.aliases.includes(args[0].toLowerCase()));
 
-        let helpCmdEmbed = new client.discord.MessageEmbed()
-          .setTitle(`${client.user.username} Help | \`${(name.toLocaleString())}\` Command`)
-          .addFields(
-            { name: "Description", value: `${description}` },
-            { name: "Usage", value: `${usage}` },
-            { name: "Aliases", value: `${aliases}` },
-            { name: 'Category', value: `${category}` })
-          .setColor(client.config.embedColor)
-          .setFooter(client.config.embedfooterText, client.user.avatarURL());
-        
-        message.reply({embeds: [helpCmdEmbed], allowedMentions: {repliedUser: false}});
-      }
-    }
-  },
+            // This is what it sends when using the command with argument and it does not find the command
+            if (!command) {
+                message.reply({ content: `There isn't any command named "${args[0]}"`, allowedMentions: { repliedUser: false } });
+            } else {
+
+                // This is what it sends when using the command with argument and if it finds the command
+                let command = client.commands.get(args[0].toLowerCase()) || client.commands.find((c) => c.aliases && c.aliases.includes(args[0].toLowerCase()));
+                let name = command.name;
+                let description = command.description || "No descrpition provided"
+                let usage = command.usage || "No usage provided"
+                let aliases = command.aliases || "No aliases provided"
+                let category = command.category || "No category provided!"
+
+                let helpCmdEmbed = new client.discord.MessageEmbed()
+                    .setTitle(`${client.user.username} Help | \`${(name.toLocaleString())}\` Command`)
+                    .addFields(
+                        { name: "Description", value: `${description}` },
+                        { name: "Usage", value: `${usage}` },
+                        { name: "Aliases", value: `${aliases}` },
+                        { name: 'Category', value: `${category}` })
+                    .setColor(client.config.embedColor)
+                    .setFooter({ text: `${client.config.embedfooterText}`, iconURL: `${client.user.displayAvatarURL()}` });
+
+                message.reply({ embeds: [helpCmdEmbed], allowedMentions: { repliedUser: false } });
+            }
+        }
+    },
 };
--- commands/Utility/ping.js
+++ commands/Utility/ping.js
@@ -1,22 +1,23 @@
 // Example of how to make a Command
 
 module.exports = {
-  name: "ping",
-  aliases: ["pong", "latency"],
-  category: "Utility",
-  description: "Check the bot's ping!",
-  run: async (client, message, args) => {
-    const msg = await message.channel.send(`🏓 Pinging...`);
+    name: "ping",
+    aliases: ["pong", "latency"],
+    category: "Utility",
+    description: "Check the bot's ping!",
+    ownerOnly: false,
+    run: async (client, message, args) => {
+        const msg = await message.channel.send(`🏓 Pinging...`);
 
-    const pingEmbed = new client.discord.MessageEmbed()
-      .setTitle(':signal_strength: Bot Ping')
-      .addField("Time", `${Math.floor(msg.createdAt - message.createdAt)}ms`, true)
-      .addField("API Ping", `${client.ws.ping}ms`, true)
-      .setColor(client.config.embedColor)
-      .setFooter(client.config.embedfooterText, client.user.avatarURL());
+        const pingEmbed = new client.discord.MessageEmbed()
+            .setTitle(':signal_strength: Bot Ping')
+            .addField("Time", `${Math.floor(msg.createdAt - message.createdAt)}ms`, true)
+            .addField("API Ping", `${client.ws.ping}ms`, true)
+            .setColor(client.config.embedColor)
+            .setFooter({ text: `${client.config.embedfooterText}`, iconURL: `${client.user.displayAvatarURL()}` });
 
-    await message.reply({embeds: [pingEmbed], allowedMentions: {repliedUser: false}});
+        await message.reply({ embeds: [pingEmbed], allowedMentions: { repliedUser: false } });
 
-    msg.delete();
-  },
+        msg.delete();
+    },
 };
