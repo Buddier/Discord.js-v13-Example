750a13ccad79e10de6604c0ea3344e84b0353524
Expectatives <79179637+Expectatives@users.noreply.github.com>
2022-03-12 21:13:32 -0500

    Change config.json to config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    botClientID: "YOUR_BOT_ID",
+    botPrefix: "!",
+    ownerID: "YOUR_ID",
+    embedColor: "#00dbff",
+    embedfooterText: "Discord.js v13 Example by Expectatives#1157"
+}
