0698256ab136af8a98187e83de2a4c6d2a5d13ae
Expectatives <79179637+Expectatives@users.noreply.github.com>
2022-03-12 21:13:11 -0500

    Fix slashcommands

--- slashcommands/Utility/ping.js
+++ slashcommands/Utility/ping.js
@@ -1,21 +1,22 @@
 // Example of how to make a SlashCommand
 
 module.exports = {
-  name: "ping",
-  category: "Utility",
-  description: "Check the bot's ping!",
-  run: async (client, interaction) => {
-    const msg = await interaction.channel.send(`🏓 Pinging...`);
+    name: "ping",
+    category: "Utility",
+    description: "Check the bot's ping!",
+    ownerOnly: false,
+    run: async (client, interaction) => {
+        const msg = await interaction.channel.send(`🏓 Pinging...`);
 
-    const pingEmbed = new client.discord.MessageEmbed()
-      .setTitle(':signal_strength: Bot Ping')
-      .addField("Time", `${Math.floor(msg.createdAt - interaction.createdAt)}ms`, true)
-      .addField("API Ping", `${client.ws.ping}ms`, true)
-      .setColor(client.config.embedColor)
-      .setFooter(client.config.embedfooterText, client.user.avatarURL());
+        const pingEmbed = new client.discord.MessageEmbed()
+            .setTitle(':signal_strength: Bot Ping')
+            .addField("Time", `${Math.floor(msg.createdAt - interaction.createdAt)}ms`, true)
+            .addField("API Ping", `${client.ws.ping}ms`, true)
+            .setColor(client.config.embedColor)
+            .setFooter({ text: `${client.config.embedfooterText}`, iconURL: `${client.user.displayAvatarURL()}` });
 
-    await interaction.followUp({embeds: [pingEmbed]});
+        await interaction.reply({ embeds: [pingEmbed] });
 
-    msg.delete();
-  },
+        msg.delete();
+    },
 };
